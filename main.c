d9430d819f194fde41aa51066188334a62e693df
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-07-25 15:10:57 -0600

    simplified find_BWE() with library function

--- main.c
+++ main.c
@@ -8,48 +8,24 @@ int main(int argc, char **argv){
 
 	libusb_device_handle *dev_handle;
 
-	find_BWE(&dev_handle);
+	find_BWE(dev_handle);
 
 	libusb_exit(NULL);
 
 	return 0;
 }
 
-int find_BWE(libusb_device_handle **dev_handle){
+int find_BWE(libusb_device_handle *dev_handle){
 
-	libusb_device **list;
-
-	int devices_length = libusb_get_device_list(NULL, &list);
-
-	libusb_device *device = NULL;
-
-	bool notfound = true;
-	for(int i = 0; i < devices_length; i++){
-		device = *(list + i);
-		struct libusb_device_descriptor desc;
-		if(libusb_get_device_descriptor(device, &desc) != 0){
-			safe_exit(list);
-			return 1;
-		}
-		if(desc.idVendor == 0x1532 && desc.idProduct == 0x0237){
-			notfound = false;
-			printf("Razer BlackWidow Essential found");
-		}
-	}
-
-	if(notfound){
-		printf("Razer BlackWidow Essential not found");
-		safe_exit(list);
+	if((dev_handle = libusb_open_device_with_vid_pid(NULL, 0x1532, 0x0237)) == NULL){
+		printf("Razer BlackWidow Essential not found or could not be opened");
 		return 1;
 	}
 
-	if(libusb_open(device, dev_handle) != 0){
-		printf("Razer BlackWidow Essential could not be opened");
-		safe_exit(list);
-		return 1;
-	}
+	return 0;
+}
 
-	libusb_free_device_list(list, 1);
+int send_request(libusb_device_handle *dev_handle){ 
 	return 0;
 }
 
--- main.h
+++ main.h
@@ -3,4 +3,5 @@
 #include "libusb-1.0/libusb.h"
 
 void safe_exit(libusb_device **list);
-int find_BWE(libusb_device_handle **dev_handle);
+int find_BWE(libusb_device_handle *dev_handle);
+int send_request(libusb_device_handle *dev_handle);
