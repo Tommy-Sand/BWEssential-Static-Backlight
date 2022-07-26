7f50af9829d83e86b91ad68b683b61f52e124d24
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-07-26 17:44:42 -0600

    finished implementation

--- main.c
+++ main.c
@@ -9,9 +9,7 @@ int main(){
 		return 1;
 	}
 	
-
 	int sent = hid_send_feature_report(device, data, sizeof(data) / sizeof(char));
-	printf("datasize: %d, sizeof(char): %d , Sent: %d", sizeof(data), sizeof(char), sent); //debugging
 
 	hid_close(device);
 	
@@ -22,7 +20,7 @@ hid_device *find_BWE(){
 	struct hid_device_info *linked_list_devices = hid_enumerate(DEV_VID, DEV_PID);
 	struct hid_device_info *device_info = linked_list_devices;
 	while(device_info != NULL){
-		if(device_info->usage_page == 0x01 && device_info->usage == 0x02 && device_info->interface_number == 0x02){
+		if(device_info->usage_page == DEV_USAGE_PAGE && device_info->usage == DEV_USAGE2 && device_info->interface_number == DEV_INTERFACE_NUMBER){
 			break;
 		}
 		device_info = device_info->next;
